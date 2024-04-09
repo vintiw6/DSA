991c83aecc7071bbfaebcc6778f8e7a55637f55e
Vinayak Tiwari <159702509+vintiw6@users.noreply.github.com>
2024-04-10 00:05:47 +0530

    edited some things

--- Searching/binsearch.c
+++ Searching/binsearch.c
@@ -1,39 +1,49 @@
 #include <stdio.h>
 
-int main() {
-    int elements , mid , key;
+int main()
+{
+    int elements, mid, key;
     int l = 0;
     int h = elements - 1;
 
-
     printf("Enter the number of elements:\n");
-    scanf("%d", &elements); 
-    if (elements <= 0) {
+    scanf("%d", &elements);
+    if (elements <= 0)
+    {
         printf("Invalid input: Number of elements must be positive.\n");
-        return 1; 
+        return 1;
     }
 
     int arr[elements];
     printf("Enter the elements in sorted format:\n");
-    for (int i = 0; i < elements; i++) {
+    for (int i = 0; i < elements; i++)
+    {
         scanf("%d", &arr[i]);
     }
     printf("Enter the searching element: ");
     scanf("%d", &key);
-    while (l <= h) {
+    while (l <= h)
+    {
         mid = (l + h) / 2;
 
-        if (arr[mid] == key) {
-            printf("%d Present at %d location.\n", key, mid + 1); 
+        if (arr[mid] == key)
+        {
+            printf("%d Present at %d location.\n", key, mid + 1);
             return 0;
-        } else if (arr[mid] < key) {
+        }
+        else if (arr[mid] < key)
+        {
             l = mid + 1;
-        } else if (arr[mid] > key){
+        }
+        else if (arr[mid] > key)
+        {
             h = mid - 1;
-        } else {
-             printf("Element Not Found.\n");
-             return 1; 
+        }
+        else
+        {
+            printf("Element Not Found.\n");
+            return 1;
         }
     }
-   return 0;
+    return 0;
 }
--- Searching/linsearch.c
+++ Searching/linsearch.c
@@ -1,21 +1,28 @@
-#include<stdio.h>
-int linsearch(int arr[],int elements, int target ){
-    for(int i=0;i<elements;i++){
-        if(arr[i]==target){
+#include <stdio.h>
+int linsearch(int arr[], int elements, int target)
+{
+    for (int i = 0; i < elements; i++)
+    {
+        if (arr[i] == target)
+        {
             return i;
         }
     }
-      return -1;
+    return -1;
 }
 
-int main(){
-    int arr[] = {10,20,30,40,50,60};
-    int elements = sizeof(arr)/sizeof(arr[0]);
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50, 60};
+    int elements = sizeof(arr) / sizeof(arr[0]);
     int target = 40;
-    int result = linsearch(arr , elements , target);
-    if(result != -1){
-        printf("The given %d element is present at Index: %d",target,result);
-    } else {
+    int result = linsearch(arr, elements, target);
+    if (result != -1)
+    {
+        printf("The given %d element is present at Index: %d", target, result);
+    }
+    else
+    {
         printf("Error");
     }
     return 0;
